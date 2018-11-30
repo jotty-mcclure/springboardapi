3bfba9ca959d0628ef42a9a4a912a1b13c4748d9
Joshua McClure <joshua.mcclure@deque.com>
2018-11-30 12:57:12 -0500

    moved the generate util to the lib dir.

--- generate/_template/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose	= require('mongoose');
-const schema    = mongoose.Schema;
-
-const Schema = new schema({
-	created: { type: Date, default: Date.now },
-	updated: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('{{singular_capitalized}}', Schema);
