b91378471ee3850325c64914f716d22d95d13a5e
Joshua McClure <josh@joshuadmcclure.com>
2018-12-02 21:00:28 -0500

    Removed the front-end crap from the repo

--- client/src/app/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home';
-export { default as Register } from './register';
-export { default as About } from './about';
-export { default as Protected } from './protected';
-export { default as Error } from './error';
