2a8ebb537a8cd0d33d1095eab1dc38f74a9eee30
Joshua McClure <josh@joshuadmcclure.com>
2018-12-06 01:16:54 -0500

    Removed unneeded files

--- server/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app, config) => {
-    app.use(`/${config.env[config.mode].apiUrlBasePath}/`, require('./modules/auth/authRouter'));
-    app.use(`/${config.env[config.mode].apiUrlBasePath}/`, require('./modules/user/userRouter'));
-    app.use(`/${config.env[config.mode].apiUrlBasePath}/`, require('./modules/projects/projectsRouter'));
-}
